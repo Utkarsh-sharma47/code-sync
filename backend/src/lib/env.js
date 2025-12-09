3e3c3378c69234140c4d1b657aebfb07f0dbdfa3
UtkarshSharma <utkarsh4.sharma005@gmail.com>
2025-12-10 00:14:10 +0530

    edit env.js

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,10 +1,8 @@
+// backend/src/lib/env.js
 import dotenv from 'dotenv';
 dotenv.config();
 
-// you dont need to import dotenv in other files now
-// just import ENV from this file and get access to your env variables
-
 export const ENV = {
-    PORT : process.env.PORT,
-    NODE_ENV : process.env.NODE_ENV
-}
+  PORT: process.env.PORT || 5000,          // 5000 for local, PORT from Render in prod
+  NODE_ENV: process.env.NODE_ENV || 'development'
+};
