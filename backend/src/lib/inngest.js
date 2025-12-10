8a3f27317ce6cd84af2af892bdd73465cbc2d0f2
UtkarshSharma <utkarsh4.sharma005@gmail.com>
2025-12-10 16:20:36 +0530

    innest js fix

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,36 +1,40 @@
-import {Inngest} from 'inngest';
-import {connectDB} from './db.js';
-import {User} from '../models/User.js';
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import { User } from "../models/User.js";
 
+export const inngest = new Inngest({ name: "code-sync-app" });
+
+// sync user
 const syncUser = inngest.createFunction(
   { id: "sync-user" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    await connectDB()
+    await connectDB();
 
-    const { id, email_addresses, first_name, last_name, image_url } = event.data
+    const { id, email_addresses, first_name, last_name, image_url } = event.data;
 
     const newUser = {
       clerkId: id,
       email: email_addresses[0]?.email_address,
-      name: `${first_name || ""} ${last_name || ""}`,
-      profileImage: image_url
-    }
+      name: `${first_name || ""} ${last_name || ""}`.trim(),
+      profileImage: image_url,
+    };
 
-    await User.create(newUser)
+    await User.create(newUser);
   }
-)
+);
 
-
+// delete user
 const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
-    await connectDB()
+    await connectDB();
 
-    const { id } = event.data
-    await User.deleteOne({ clerkId: id })
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
   }
-)
+);
 
+// Export both so server.js can use them
 export const functions = [syncUser, deleteUserFromDB];
